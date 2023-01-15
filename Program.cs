55db3e9a980e15b074d0626cf3e283ca3f945ac1
kyq <lymkal.kotov@yandex.ru>
2023-01-15 10:40:41 +0300

    Добавили тело цикла,в котором код ищет подходящие под наши условия значения массива

--- Program.cs
+++ Program.cs
@@ -1,16 +1,7 @@
 ﻿string[] M;
             Console.WriteLine("Введите массив строк через пробел");
-            string stroka = Console.ReadLine();
+            string stroka = Console.ReadLine(); 
             M = stroka.Split(' ');
-            var result = new string[M .Length];
+            var result = new string[M .Lenght];
             var realSize = 0;
-            foreach (var value in M )
-            {
-                if (value.Length <= 3)
-                {
-                    result[realSize] = value;
-                    realSize++;
-                }
-            }
-            Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
-            Console.ReadKey(true);
+            
